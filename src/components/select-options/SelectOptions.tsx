3cb31b9e49639d22ee406bea62a63876192f0b12
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-05-20 10:53:38 +0330

    feat: set validation message on the input number

--- src/components/select-options/SelectOptions.tsx
+++ src/components/select-options/SelectOptions.tsx
@@ -1,28 +1,38 @@
+import { useState } from "react";
 import { useOptionsContext } from "../../context/OptionsContext";
 
 export function SelectOptions() {
+  const [amountError, setAmountError] = useState("");
   const { optionsState, optionsDispatch } = useOptionsContext();
-  const handleCategory =(selected)=>{
-    optionsDispatch({type:"CHANGE_CATEGORY",payload: selected })
-  }
-  
-  const handleDifficulty=(selected)=>{
-    optionsDispatch({type:"CHANGE_DIFFICULTY",payload: selected })
-  }
+  const handleCategory = (selected) => {
+    optionsDispatch({ type: "CHANGE_CATEGORY", payload: selected });
+  };
 
-  const handleAmount=(selected)=>{
-    optionsDispatch({type:"CHANGE_AMOUNT",payload: selected })
-  }
+  const handleDifficulty = (selected) => {
+    optionsDispatch({ type: "CHANGE_DIFFICULTY", payload: selected });
+  };
+
+  const handleAmount = (selected) => {
+    optionsDispatch({ type: "CHANGE_AMOUNT", payload: selected });
+    if (selected < 50 && selected > 5) {
+      optionsDispatch({type:"IS_VALID",payload:true})
+    }
+  };
 
   return (
     <div className="flex flex-col gap-4">
       <label htmlFor="number" className="flex flex-col">
         Number of Question
-        <input type="number" id="number" onChange={(e)=>handleAmount(e.target.value)}/>
+        <input
+          type="number"
+          id="number"
+          onChange={(e) => handleAmount(e.target.value) }
+        />
+        <p>{optionsState.invalidMessage}</p>
       </label>
       <label htmlFor="category" className="flex flex-col">
         Category
-        <select onChange={(e)=>handleCategory(e.target.value)}>
+        <select onChange={(e) => handleCategory(e.target.value)}>
           <option value="category">Category</option>
           <option value="25">Art</option>
           <option value="12">Music</option>
@@ -31,7 +41,7 @@ export function SelectOptions() {
       </label>
       <label htmlFor="difficulty" className="flex flex-col">
         Difficulty
-        <select onChange={(e)=>handleDifficulty(e.target.value)} >
+        <select onChange={(e) => handleDifficulty(e.target.value)}>
           <option value="difficulty">Difficulty</option>
           <option value="easy">Easy</option>
           <option value="medium">Medium</option>
--- src/components/setup-section/SetupSection.tsx
+++ src/components/setup-section/SetupSection.tsx
@@ -6,27 +6,44 @@ import { useOptionsContext } from "../../context/OptionsContext";
 
 export function SetupSection() {
   const [data, setData] = useState(null);
-  const [start, setStart]=useState(false);
+  const [start, setStart] = useState(false);
   const { optionsState, optionsDispatch } = useOptionsContext();
 
   useEffect(() => {
     if (start) {
-      get(optionsState.amount,optionsState.category, optionsState.difficulty).then((res) => {
-        console.log(res,optionsState.category, optionsState.difficulty)
-        setData(res);
-        setStart(false); // Reset the start state
-      });
+      if (!optionsState.valid) {
+        optionsDispatch({
+          type: "SET_INVALID_MESSAGE",
+          payload: "Please Enter Number Between 5 And 50",
+        });
+        setStart(false);
+      } else {
+        get(
+          optionsState.amount,
+          optionsState.category,
+          optionsState.difficulty
+        ).then((res) => {
+          setData(res);
+          optionsDispatch({ type: "SET_DATA", payload: res.results });
+          setStart(false); // Reset the start state
+        });
+        optionsDispatch({ type: "CHANGE_PAGE", payload: 2 });
+      }
     }
-  }, [start]);
-    console.log(data);
- 
+  }, [start, optionsState.valid, optionsDispatch]);
 
   return (
-    <div className="bg-dark_purple rounded-md  w-1/2 h-4/5">
+    <div className="bg-purple-600 rounded-md  w-1/2 h-4/5">
       <h1>welcome</h1>
       <p>start</p>
       <SelectOptions />
-      <button onClick={()=>setStart(true)}>start</button>
+      <button
+        onClick={() => {
+          setStart(true);
+        }}
+      >
+        start
+      </button>
     </div>
   );
 }
