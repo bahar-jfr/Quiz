b35e6e4c98c319628fe6d5e8fe9a77bd58dc0c40
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-05-24 14:39:27 +0330

    style: set theme color

--- src/components/select-options/SelectOptions.tsx
+++ src/components/select-options/SelectOptions.tsx
@@ -1,47 +1,53 @@
-import { useState } from "react";
+
 import { useOptionsContext } from "../../context/OptionsContext";
+import { useThemeContext } from "../../context/ThemeContext";
 
 export function SelectOptions() {
-  const [amountError, setAmountError] = useState("");
   const { optionsState, optionsDispatch } = useOptionsContext();
-  const handleCategory = (selected) => {
+  const { themeState } = useThemeContext();
+  const handleCategory = (selected:string) => {
     optionsDispatch({ type: "CHANGE_CATEGORY", payload: selected });
   };
 
-  const handleDifficulty = (selected) => {
+  const handleDifficulty = (selected:string) => {
     optionsDispatch({ type: "CHANGE_DIFFICULTY", payload: selected });
   };
 
-  const handleAmount = (selected) => {
-    optionsDispatch({ type: "CHANGE_AMOUNT", payload: selected });
-    if (selected < 50 && selected > 5) {
+  const handleAmount = (selected:string) => {
+    optionsDispatch({ type: "CHANGE_AMOUNT", payload: +selected });
+    if (+selected <= 50 && +selected >= 5) {
       optionsDispatch({type:"IS_VALID",payload:true})
     }
   };
 
   return (
-    <div className="flex flex-col gap-4">
-      <label htmlFor="number" className="flex flex-col">
+    <div className="flex flex-col gap-1.5 w-full">
+      <label htmlFor="number" className="flex flex-col gap-0.5">
         Number of Question
         <input
           type="number"
           id="number"
-          onChange={(e) => handleAmount(e.target.value) }
+          onChange={(e) => {handleAmount(e.target.value)
+            if (+e.target.value <= 50 && +e.target.value >= 5) {
+              optionsDispatch({type:"IS_VALID",payload:true})
+            }
+          } }
+          className={`rounded-md py-1.5 px-3.5 outline-none  ${themeState.mode === "dark" ? "bg-dark_select text-dark_text" : "bg-light_select"}`}
         />
-        <p>{optionsState.invalidMessage}</p>
+        <p className="text-red-700 text-xs">{optionsState.invalidMessage}</p>
       </label>
-      <label htmlFor="category" className="flex flex-col">
+      <label htmlFor="category" className="flex flex-col gap-0.5">
         Category
-        <select onChange={(e) => handleCategory(e.target.value)}>
+        <select className={`rounded-md py-1.5 px-3 ${themeState.mode === "dark" ? "bg-dark_select text-dark_text" : "bg-light_select"}`}onChange={(e) => handleCategory(e.target.value)}>
           <option value="category">Category</option>
           <option value="25">Art</option>
           <option value="12">Music</option>
           <option value="32">Cartoon & Animations</option>
         </select>
       </label>
-      <label htmlFor="difficulty" className="flex flex-col">
+      <label htmlFor="difficulty" className="flex flex-col gap-0.5">
         Difficulty
-        <select onChange={(e) => handleDifficulty(e.target.value)}>
+        <select className={`rounded-md py-1.5 px-3 ${themeState.mode === "dark" ? "bg-dark_select text-dark_text" : "bg-light_select"}`}onChange={(e) => handleDifficulty(e.target.value)}>
           <option value="difficulty">Difficulty</option>
           <option value="easy">Easy</option>
           <option value="medium">Medium</option>
