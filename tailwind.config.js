2c0e8284fcb60430ee34c1d5f50384e316580546
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-05-24 14:24:10 +0330

    style: declare theme color extends

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      invert:{
-        "20":"0.2"
+      invert: {
+        20: "0.2",
+      },
+      colors:{
+        light_root : "#d8f3dc",
+        light_main_box:"#74c69d",
+        light_question:"#c4fff9",
+        light_select : "#c9184a",
+        light_btn:"#40f9c8",
+        light_text: "#08260A",
+        dark_root : "#16425b",
+        dark_main_box:"#3a7ca5",
+        dark_question:"#3c096c",
+        dark_select : "#fbde21",
+        dark_text:"#533131",
+        dark_btn:"#5a189a"
       }
     },
   },
   plugins: [],
-}
+};
