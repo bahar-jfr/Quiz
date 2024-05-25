a1aa7b3a27f20b396502b54d5b2af0bc90433c88
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-05-25 21:16:09 +0330

    style: add color and animation to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,37 @@ export default {
       invert: {
         20: "0.2",
       },
-      colors:{
-        light_root : "#d8f3dc",
-        light_main_box:"#74c69d",
-        light_question:"#c4fff9",
-        light_select : "#c9184a",
-        light_btn:"#40f9c8",
+      colors: {
+        light_root: "#d8f3dc",
+        light_main_box: "#74c69d",
+        light_question: "#c4fff9",
+        light_select: "#c9184a",
+        light_btn: "#40f9c8",
         light_text: "#08260A",
-        dark_root : "#16425b",
-        dark_main_box:"#3a7ca5",
-        dark_question:"#3c096c",
-        dark_select : "#fbde21",
-        dark_text:"#533131",
-        dark_btn:"#5a189a"
-      }
+        light_hover: "#92d3b3",
+        dark_root: "#16425b",
+        dark_main_box: "#3a7ca5",
+        dark_question: "#3c096c",
+        dark_select: "#fbde21",
+        dark_text: "#533131",
+        dark_btn: "#5a189a",
+        dark_hover: "#2d6080",
+        dark_shadow: "#102f42",
+      },
+      boxShadow: {
+        dark: "0px 0px 17px 3px rgba(15, 44, 62,1)",
+        light: "0px 0px 17px 3px rgba(57,131,94,1)",
+      },
+      keyframes: {
+        rocket: {
+          "0%": { transform: "translateY(0) rotate(0deg)" },
+          "50%": { transform: "translateY(-3cm) rotate(0deg)" },
+          "100%": { transform: "translateY(0) rotate(180deg)" },
+        },
+      },
+      animation: {
+        rocket: "rocket 2s ease-in-out",
+      },
     },
   },
   plugins: [],
