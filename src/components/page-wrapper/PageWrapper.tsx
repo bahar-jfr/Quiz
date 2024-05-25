9a4df599c12920d03e810c8ea7a2311abfadfe03
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-05-25 21:13:54 +0330

    feat: pages routes

--- src/components/page-wrapper/PageWrapper.tsx
+++ src/components/page-wrapper/PageWrapper.tsx
@@ -1,13 +1,14 @@
 import { useThemeContext } from "../../context/ThemeContext";
 import { MdOutlineBrightnessMedium } from "react-icons/md";
-import { SetupSection } from "../setup-section/SetupSection";
+import { SetupSection } from "../../pages/setup-section/SetupSection";
 import { useOptionsContext } from "../../context/OptionsContext";
-import { IntroSection } from "../intro-section/IntroSection";
-import { MainPage } from "../../pages/main-page/MainPage";
+import { IntroSection } from "../../pages/intro-section/IntroSection";
+import {  MainSection } from "../../pages/main-section/MainSection";
+import { ResultSection } from "../../pages/result-section/ResultSection";
 
 export function PageWrapper() {
   const { themeState, themeDispatch } = useThemeContext();
-  const { optionsState, optionsDispatch } = useOptionsContext();
+  const { optionsState } = useOptionsContext();
   const toggleMode = () => {
     themeDispatch({
       type: "CHANGE_THEME",
@@ -18,23 +19,26 @@ export function PageWrapper() {
   return (
     <div
       className={`flex flex-col items-center gap-8 h-screen  ${
-        themeState.mode === "dark" ? "bg-gray-600" : "bg-slate-100"
+        themeState.mode === "dark" ? "bg-dark_root" : "bg-light_root"
       }`}
     >
-      <div className="text-left pt-4 px-8 w-full">
+      <div className="text-left pt-6 px-8 w-full ">
         <MdOutlineBrightnessMedium
           onClick={toggleMode}
-          className={themeState.mode === "dark" ? "invert" : "invert-20"}
+          className={themeState.mode === "dark" ? "invert size-6" : "invert-20 size-6"}
         />
       </div>
-      
+
       {optionsState.page === 0 ? (
         <IntroSection />
       ) : optionsState.page === 1 ? (
         <SetupSection />
       ) : optionsState.page === 2 ? (
-        <MainPage />
-      ) : null}
+        <MainSection />
+      ) :optionsState.page ===3?(
+        <ResultSection/>
+      )
+       : null}
     </div>
   );
 }
