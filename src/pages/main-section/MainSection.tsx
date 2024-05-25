cc262bbbe6f0bad39abb173bd8352aab3c963720
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-05-25 21:12:39 +0330

    style: set theme color

--- src/pages/main-section/MainSection.tsx
+++ src/pages/main-section/MainSection.tsx
@@ -1,43 +1,48 @@
 import { useEffect, useState } from "react";
 import { useOptionsContext } from "../../context/OptionsContext";
 import { useThemeContext } from "../../context/ThemeContext";
+import { TitleLogo } from "../../components/title-logo/TitleLogo";
+import { CgSpinner } from "react-icons/cg";
+
 
 export function MainSection() {
   const { optionsState, optionsDispatch } = useOptionsContext();
   const { themeState } = useThemeContext();
-  const [question, setQuestion] = useState("");
+  const [question, setQuestion] = useState<string>("");
   const [choices, setChoices] = useState<string[]>([]);
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState<number>(0);
+
+  type QuestionData = {
+    question: string;
+    correct_answer: string;
+    incorrect_answers: string[];
+  };
+
+  //iterate over the array from the last element to the first
+  const shuffleArray = (array: string[]): string[] => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
 
   useEffect(() => {
     if (optionsState.data && optionsState.data.length > 0) {
-      setQuestion(optionsState.data[index].question);
+      const currentQuestion: QuestionData = optionsState.data[index];
+      setQuestion(currentQuestion.question);
 
-      const correctAnswer = optionsState.data[index].correct_answer;
-      const incorrectAnswes = optionsState.data[index].incorrect_answers;
+      const correctAnswer = currentQuestion.correct_answer;
+      const incorrectAnswers = currentQuestion.incorrect_answers;
 
-      //combine correct and incorrect answers in one array
-      const allChoices = [correctAnswer, ...incorrectAnswes];
-
-      //iterate over the array from the last element to the first
-      const shuffleArray = (array) => {
-        for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-      };
-
-      //randomize array / shuffle array
+      const allChoices = [correctAnswer, ...incorrectAnswers];
       const shuffledChoices = shuffleArray(allChoices);
 
       setChoices(shuffledChoices);
     }
   }, [optionsState.data, index]);
 
-  console.log(optionsState.data[index]);
-
-  const handleSelectChoice = (choosen) => {
+  const handleSelectChoice = (choosen: string) => {
     if (index + 1 === optionsState.data.length) {
       optionsDispatch({ type: "CHANGE_PAGE", payload: 3 });
     } else {
@@ -54,26 +59,33 @@ export function MainSection() {
 
   return (
     <div
-      className={` rounded-md flex flex-col gap-6 items-center pt-8 w-1/2 h-4/5 ${
+      className={` rounded-md flex flex-col gap-8 items-center pt-12 w-1/2 h-4/5 ${
         themeState.mode === "dark"
-          ? "bg-dark_main_box text-white"
-          : "bg-light_main_box text-light_text"
+        ? "bg-dark_main_box text-white shadow-dark"
+        : "bg-light_main_box text-light_text shadow-light"
       }`}
     >
-      <p className="font-bold text-2xl"> Quiz</p>
-      
-   <div className={`  rounded-md w-5/6 px-3 py-3 ${themeState.mode === "dark" ? "bg-dark_question" : "bg-light_question"}`}>   {question ? <p >{question}</p> : <p>Loading ...</p>}</div>
+      <TitleLogo/>
+
+      <div
+        className={`  rounded-md w-5/6 px-3 py-3 mt-4 ${
+          themeState.mode === "dark" ? "bg-dark_question" : "bg-light_question"
+        }`}
+      >
+        {" "}
+        {question ? <p>{question}</p> : <p className="flex items-center gap-2"><CgSpinner className="animate-spin" /> Loading ...</p>}
+      </div>
       <div className="flex flex-col gap-2.5 px-24 w-full">
         {" "}
         {choices.map((choice, index) => (
           <button
             key={index}
-            className={`flex rounded-md py-1.5 px-3  ${  themeState.mode === "dark" ? "bg-dark_btn" : "bg-light_btn"}`
-            
-            }
+            className={`flex rounded-md py-1.5 px-3  ${
+              themeState.mode === "dark" ? "bg-dark_btn" : "bg-light_btn"
+            }`}
             onClick={() => handleSelectChoice(choice)}
           >
-            {index+1}. {choice}
+            {index + 1}. {choice}
           </button>
         ))}
       </div>
